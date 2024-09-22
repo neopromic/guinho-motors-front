72214406ad0294c75297f16998799e2c35329037
Wesley Souza | Neo <cusaowesley1@gmail.com>
2024-09-22 17:55:52 -0300

    feat: add a leading text on featured cars section

--- app/page.tsx
+++ app/page.tsx
@@ -9,8 +9,9 @@ import {
   SelectValue,
 } from '@/components/ui/select'
 import { VehicleCard, type IVehicleProps } from '@/components/VehicleCard'
-import { Search } from 'lucide-react'
+import { Flame, Search } from 'lucide-react'
 import Image from 'next/image'
+import './app.css'
 
 export default function Home() {
   const cars: IVehicleProps[] = [
@@ -46,7 +47,7 @@ export default function Home() {
       name: 'Nissan GT-R',
       vehicleType: 'Sport',
       price: 'R$ 215.900,00',
-      imageURl: '/banner_car.png',
+      imageURl: '/banner_car_2.png',
       fuel: '80L',
       carMode: 'Automático',
     },
@@ -54,7 +55,7 @@ export default function Home() {
 
   return (
     <main className="h-full scroll-smooth bg-muted">
-      <div className="px-4">
+      <div className="bg-muted px-4">
         <Card className="mt-4 flex flex-col items-center justify-between gap-2 overflow-hidden rounded-[0.88rem] bg-gradient-to-r from-primary via-red-700 to-red-800 px-4 py-6 text-white shadow-md transition-all hover:shadow-lg lg:min-h-64 lg:flex-row">
           <CardHeader className="flex flex-col space-y-4">
             <h1 className="max-w-md text-4xl font-semibold leading-none tracking-tight">
@@ -82,7 +83,7 @@ export default function Home() {
           </CardContent>
         </Card>
       </div>
-      <section className="my-4 px-12">
+      <section className="my-4 w-full px-4">
         <div className="rounded-[0.88rem] bg-white p-4">
           <div className="flex items-center gap-3">
             <div className="size-2 rounded-full bg-primary ring ring-indigo-300" />
@@ -125,6 +126,10 @@ export default function Home() {
           </div>
         </div>
       </section>
+      <div className="flex w-full gap-2 p-4 text-muted-foreground">
+        <Flame />
+        <h1>Mais comprados</h1>
+      </div>
       <section className="grid grid-cols-1 place-items-center items-center gap-2 px-4 md:grid-cols-2 lg:grid-cols-2">
         {cars.map(c => (
           <VehicleCard

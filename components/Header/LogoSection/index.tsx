6da795c262605d54cb4e7235e31bc853197c8955
Wesley Souza | Neo <cusaowesley1@gmail.com>
2024-09-28 16:13:00 -0300

    feat: add link to header logo

--- components/Header/LogoSection/index.tsx
+++ components/Header/LogoSection/index.tsx
@@ -1,9 +1,13 @@
+import Link from "next/link"
+
 export const LogoSection = () => {
   return (
     <div className="">
+      <Link href={"/"}>
       <h1 className="font-medium leading-none tracking-tighter text-primary">
         Guinho <br /> Motors
       </h1>
+      </Link>
     </div>
   )
 }
